86db71f7c95cc8c7afe5e17bac8cc86ba9c39088
sergio <sergiopopov@gmail.com>
2025-01-25 01:01:58 +0200

    Implemtn configs and DB

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	todo "github.com/SergioPopovs176/my-todo"
 	"github.com/SergioPopovs176/my-todo/pkg/handler"
 	"github.com/SergioPopovs176/my-todo/pkg/repository"
 	"github.com/SergioPopovs176/my-todo/pkg/service"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	fmt.Println("Config loaded !")
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.SetConfigFile("configs/config.yml")
+	// viper.AddConfigPath("configs")
+
+	return viper.ReadInConfig()
 }
---
+++ pkg/repository/postgres.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDB(c Config) (*sqlx.DB, error) {
+	// Connect opens connectin to DB and cal ping function
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		c.Host, c.Port, c.Username, c.DBName, c.Password, c.SSLMode))
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
